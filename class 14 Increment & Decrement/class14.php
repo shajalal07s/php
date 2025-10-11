550cab93a71be0f098ba23db1fa7114795cedf2e
khanranu81@gmail.com <khanranu81@gmail.com>
2025-10-11 06:47:09 +0600

    Class 14 Increment And Decrement ++ & --

--- class 14 Increment & Decrement/class14.php
+++ class 14 Increment & Decrement/class14.php
@@ -5,10 +5,44 @@
     // Increment Operator
     // Increment operator is used to increase the value of a variable by one.
     // There are two types of increment operators:
-    $a = 10;
-    $a++; // Post-increment
-    echo "The value of a is : $a <br>";
+    
+    // postfix increment operator
+    $_amount = 10;
+    
+    // Postfix Increment Return kore old value ++
+    echo "Postfix Old Value Return ++ : " .$_amount++; // 10
+    echo "<br>";
+    echo "Postfix Old value final result ++ : ". $_amount . "<br>"; // 11
 
+    // Postfix decrement operator
+    $_amount_minus = 20;
+
+    // Postfix decrement Return kore old value --
+    echo "Postfix Old Value Return -- : " . $_amount_minus--; // 20
+    echo "<br>";
+    echo "Postfix Old value final result -- : ". $_amount_minus;  // 19
+
+
+    /* ======================================
+      Prefix Increment and Decrement Operators
+    ========================================= */
+    
+    // Prefix potome ++ return kore new value dei
+    // Prefix Increment Return kore new value ++ 
+    echo "<br>";
+    echo "<br>";
+    $_amount2 = 15;
+
+    // Prefix Increment Return kore new value ++
+    echo "++ Prefix New Value Return  : " .++$_amount2; // 16
+    echo "<br>";
+    echo "++ Prefix New value final result : ". $_amount2 . "<br>"; // 16
+
+    // Prefix Decrement Return kore new value --
+    $_amount_minus2 = 25;
+    echo "-- Prefix New Value Return  : " .--$_amount_minus2; // 24
+    echo "<br>";
+    echo "-- Prefix New value final result : ". $_amount_minus2; // 24
 
 
 
